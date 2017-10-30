6eaa174db5b725652c51838a5d254c5ec3eb7b0c
Melanie Downing <sayanything830@gmail.com>
2017-10-30 16:30:40 -0700

    more javascript added

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var imageArr = [
+  'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'
+];
+var imgObjArr = [];
+var imgOne;
+var imgTwo;
+var imgThree;
+var prevOne;
+var prevTwo;
+var prevThree;
+
+function ImageArrToObject(name) {
+  this.name = name;
+  this.path = 'img/' + name;
+  this.imageClicked = 0;
+  this.imageShown = 0;
+};
+
+for (var i = 0; i < imageArr.length; i ++) {
+  imgObjArr.push(new ImageArrToObject(imageArr[i]));
+}
+
+function randNumGen() {
+  var min = 0;
+  var max = imageArr.length - 1;
+  var randNum = Math.floor(Math.random() * (max - min) + 1);
+  while (randNum === prevOne || randNum === prevTwo || randNum === prevThree) {
+    randNum = Math.floor(Math.random() * imageArr.length);
+  }
+  console.log('random number', randNum);
+  return randNum;
+};
+
+function compareImg() {
+  imgOne = randNumGen();
+  imgTwo = randNumGen();
+  imgThree = randNumGen();
+  while (imgTwo === imgOne) {
+    imgTwo = randNumGen();
+  }
+  while (imgThree === imgOne || imgThree === imgTwo) {
+    imgThree = randNumGen();
+  }
+  prevOne = imgOne;
+  prevTwo = imgTwo;
+  prevThree = imgThree;
+};
+
+function loadImage() {
+  for (var j = 0; j < 25; j ++) {
+    compareImg();
+    var webArr = [];
+    webArr.push(imgObjArr[imgOne]);
+    webArr.push(imgObjArr[imgTwo]);
+    webArr.push(imgObjArr[imgThree]);
+    imgObjArr[imgOne].imageClicked++;
+    imgObjArr[imgTwo].imageClicked++;
+    imgObjArr[imgThree].imageClicked++;
+    var webEl = document.getElementById('images');
+    for (var k = 0; k < webArr.length; k ++) {
+      webEl.innerHTML = '<li><img src="' + imgObjArr[k].path + '"></li>';
+    }
+  }
+};
+loadImage();
