2cb7ebf9c66362650988549cefdf715ab95ca074
Melanie Downing <sayanything830@gmail.com>
2017-10-31 13:15:26 -0700

    got image clicked totals working and adding charts

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ for (var i = 0; i < imageArr.length; i ++) { //calls constructor function for ea
 
 function randNumGen() { //generates random number from 0-19 to become index number of image object array
   var min = 0;
-  var max = imageArr.length - 1;
-  var randNum = Math.floor(Math.random() * (max - min) + 1);
+  var max = imageArr.length;
+  var randNum = Math.floor(Math.random() * (max - min) + min);
   while (randNum === prevOne || randNum === prevTwo || randNum === prevThree) { //conditionals to prevent previous image from showing
-    randNum = Math.floor(Math.random() * (max - min) + 1);
+    randNum = Math.floor(Math.random() * (max - min) + min);
   }
   //console.log('random number', randNum);
   return randNum;
@@ -54,30 +54,37 @@ function compareImg() { //compares each index number to make sure same image doe
   prevThree = imgThree;
 };
 
+// var displayArr = [];
 function loadImage() { //this function assigns the image to a specified list item and displays in the DOM
   compareImg();
   var webArr = [];
   webArr.push(imgObjArr[imgOne]); //pushes each image to an array
   webArr.push(imgObjArr[imgTwo]);
   webArr.push(imgObjArr[imgThree]);
+  // displayArr.push(imgObjArr[imgOne]);
+  // displayArr.push(imgObjArr[imgTwo]);
+  // displayArr.push(imgObjArr[imgThree]);
   imgObjArr[imgOne].imageShown++; //adds to counter for each time image is shown
   imgObjArr[imgTwo].imageShown++;
   imgObjArr[imgThree].imageShown++;
-  webListOne.innerHTML = '<img src="' + webArr[0].path + '">'; //enters specific array item into DOM
-  webListTwo.innerHTML = '<img src="' + webArr[1].path + '">';
-  webListThree.innerHTML = '<img src="' + webArr[2].path + '">';
+  webListOne.innerHTML = '<img id="' + imgOne + '" src="' + webArr[0].path + '">'; //enters specific array item into DOM
+  webListTwo.innerHTML = '<img id="' + imgTwo + '" src="' + webArr[1].path + '">';
+  webListThree.innerHTML = '<img id="' + imgThree + '" src="' + webArr[2].path + '">';
   totalClicks++; //adds to counter of total clicks
   console.log('total clicks', totalClicks);
   console.log('img 1 shown', imgObjArr[imgOne].imageShown);
   console.log('img 2 shown', imgObjArr[imgTwo].imageShown);
   console.log('img 3 shown', imgObjArr[imgThree].imageShown);
+  //console.log(displayArr[0]);
 }
 function clickOnImage(event) { //event handler function that tells page to pull up new images until total clicks has reached 25
-  console.log(event.target);
+  console.log(event.target.id);
+  var k = event.target.id;
+  imgObjArr[k].imageClicked++;
   if (totalClicks < 26) { //total clicks starts at one once first page is loaded, so 26-1=25
     loadImage();//keep runing function
   } else { //after 25 clicks...
-    alert('That\s the end our research, thank you for your participation!');
+    //alert('That\'s the end our research, thank you for your participation!');
     displayTotals();
     function displayTotals(){ //turns each image object into a list item diplaying total times each one is shown and how many times it was clicked on
       var imgTotals = '';
@@ -87,21 +94,35 @@ function clickOnImage(event) { //event handler function that tells page to pull 
       tally.innerHTML = imgTotals; //adds to list in DOM
       //tally.appendChild(imgTotals);
       console.log('image total lists', imgTotals);
+      //This is where I add a chart
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+          labels: [
+            imgObjArr[0].name, imgObjArr[1].name, imgObjArr[2].name, imgObjArr[3].name, imgObjArr[4].name, imgObjArr[5].name, imgObjArr[6].name, imgObjArr[7].name, imgObjArr[8].name, imgObjArr[9].name, imgObjArr[10].name, imgObjArr[11].name, imgObjArr[12].name, imgObjArr[13].name, imgObjArr[14].name, imgObjArr[15].name, imgObjArr[16].name, imgObjArr[17].name, imgObjArr[18].name, imgObjArr[19].name
+          ],
+          datasets: [{
+            label: 'How many times an image was shown',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [imgObjArr[0].imageShown, imgObjArr[1].imageShown, imgObjArr[2].imageShown, imgObjArr[3].imageShown, imgObjArr[4].imageShown, imgObjArr[5].imageShown, imgObjArr[6].imageShown, imgObjArr[7].imageShown, imgObjArr[8].imageShown, imgObjArr[9].imageShown, imgObjArr[10].imageShown, imgObjArr[11].imageShown, imgObjArr[12].imageShown, imgObjArr[13].imageShown, imgObjArr[14].imageShown, imgObjArr[15].imageShown, imgObjArr[16].imageShown, imgObjArr[17].imageShown, imgObjArr[18].imageShown,imgObjArr[19].imageShown],
+          }]
+        },
+
+        // Configuration options go here
+        options: {}
+      });
+
     }
   }
 }
-loadImage(); //calls initial images to load
-addImageClicks(); //calling on event listener conditions... this is not working as planned
 
-function addImageClicks () { //event listener working, but not properly adding up each time an image was clicked
-  if (webListOne.addEventListener('click', clickOnImage)) {
-    event.target.imageClicked++;//this approach isn't working...yet.
-    //console.log('image clicked', event.target.imageClicked);
-  }
-  if (webListTwo.addEventListener('click', clickOnImage)) {
-    imgObjArr[imgTwo].imageClicked++;//nor is this one.
-  }
-  if (webListThree.addEventListener('click', clickOnImage)) {
-    imgObjArr[imgThree].imageClicked++;
-  }
-}
+loadImage(); //calls initial images to load
+
+webListOne.addEventListener('click', clickOnImage);
+webListTwo.addEventListener('click', clickOnImage);
+webListThree.addEventListener('click', clickOnImage);
