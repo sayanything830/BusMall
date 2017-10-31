06d354849d0098014837d2044cae0b3e67d032f7
Melanie Downing <sayanything830@gmail.com>
2017-10-31 14:20:09 -0700

    made comparison bar chart

--- app.js
+++ app.js
@@ -96,26 +96,33 @@ function clickOnImage(event) { //event handler function that tells page to pull 
       console.log('image total lists', imgTotals);
       //This is where I add a chart
       var ctx = document.getElementById('myChart').getContext('2d');
-      var chart = new Chart(ctx, {
+      var chartTotals = new Chart(ctx, {
         // The type of chart we want to create
         type: 'bar',
-
-        // The data for our dataset
         data: {
-          labels: [
-            imgObjArr[0].name, imgObjArr[1].name, imgObjArr[2].name, imgObjArr[3].name, imgObjArr[4].name, imgObjArr[5].name, imgObjArr[6].name, imgObjArr[7].name, imgObjArr[8].name, imgObjArr[9].name, imgObjArr[10].name, imgObjArr[11].name, imgObjArr[12].name, imgObjArr[13].name, imgObjArr[14].name, imgObjArr[15].name, imgObjArr[16].name, imgObjArr[17].name, imgObjArr[18].name, imgObjArr[19].name
-          ],
-          datasets: [{
-            label: 'How many times an image was shown',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [imgObjArr[0].imageShown, imgObjArr[1].imageShown, imgObjArr[2].imageShown, imgObjArr[3].imageShown, imgObjArr[4].imageShown, imgObjArr[5].imageShown, imgObjArr[6].imageShown, imgObjArr[7].imageShown, imgObjArr[8].imageShown, imgObjArr[9].imageShown, imgObjArr[10].imageShown, imgObjArr[11].imageShown, imgObjArr[12].imageShown, imgObjArr[13].imageShown, imgObjArr[14].imageShown, imgObjArr[15].imageShown, imgObjArr[16].imageShown, imgObjArr[17].imageShown, imgObjArr[18].imageShown,imgObjArr[19].imageShown],
-          }]
+          labels: [imgObjArr[0].name, imgObjArr[1].name, imgObjArr[2].name, imgObjArr[3].name, imgObjArr[4].name, imgObjArr[5].name, imgObjArr[6].name, imgObjArr[7].name, imgObjArr[8].name, imgObjArr[9].name, imgObjArr[10].name, imgObjArr[11].name, imgObjArr[12].name, imgObjArr[13].name, imgObjArr[14].name, imgObjArr[15].name, imgObjArr[16].name, imgObjArr[17].name, imgObjArr[18].name, imgObjArr[19].name],
+          datasets: [
+            {
+              label: 'How many times an image was shown',
+              backgroundColor: 'rgb(255, 99, 132)',
+              data: [imgObjArr[0].imageShown, imgObjArr[1].imageShown, imgObjArr[2].imageShown, imgObjArr[3].imageShown, imgObjArr[4].imageShown, imgObjArr[5].imageShown, imgObjArr[6].imageShown, imgObjArr[7].imageShown, imgObjArr[8].imageShown, imgObjArr[9].imageShown, imgObjArr[10].imageShown, imgObjArr[11].imageShown, imgObjArr[12].imageShown, imgObjArr[13].imageShown, imgObjArr[14].imageShown, imgObjArr[15].imageShown, imgObjArr[16].imageShown, imgObjArr[17].imageShown, imgObjArr[18].imageShown,imgObjArr[19].imageShown
+              ]
+            },
+            {
+              label: 'How many times an image was clicked',
+              backgroundColor: 'rgb(0, 110, 242)',
+              data: [imgObjArr[0].imageClicked, imgObjArr[1].imageClicked, imgObjArr[2].imageClicked, imgObjArr[3].imageClicked, imgObjArr[4].imageClicked, imgObjArr[5].imageClicked, imgObjArr[6].imageClicked, imgObjArr[7].imageClicked, imgObjArr[8].imageClicked, imgObjArr[9].imageClicked, imgObjArr[10].imageClicked, imgObjArr[11].imageClicked, imgObjArr[12].imageClicked, imgObjArr[13].imageClicked, imgObjArr[14].imageClicked, imgObjArr[15].imageClicked, imgObjArr[16].imageClicked, imgObjArr[17].imageClicked, imgObjArr[18].imageClicked,imgObjArr[19].imageClicked]
+            }
+          ]
         },
-
-        // Configuration options go here
-        options: {}
+        options: {
+          title: {
+            display: true,
+            text: 'Images Clicked vs. Shown'
+          }
+        }
       });
+
 
     }
   }
